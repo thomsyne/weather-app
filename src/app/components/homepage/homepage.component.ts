046c0fc9357e5b2c75972a18f32e239ef90dc245
Oluwatomisin Taiwo <oluwatomisinoluwafemi@gmail.com>
2021-05-21 23:49:03 +0100

    homepage updates

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -42,16 +42,6 @@ export class HomepageComponent implements OnInit {
 
    }
 
-   executeListing(spliced){
-     if(spliced?.length > 9){
-      const historyItems = spliced;
-      this.historys = [...historyItems].splice(this.start, this.stop);
-     } else {
-       this.historys = spliced;
-     }
-    
-   }
-
    addHistory(payload){
      this.store.dispatch({
        type: 'ADD_HISTORY',
@@ -65,8 +55,8 @@ export class HomepageComponent implements OnInit {
   ngOnInit() {
     this.subscription = this.store.select(state => state.history).subscribe(
       (response)=> {
-        var spliced = response;
-        this.executeListing(spliced);
+        console.log(response);
+        this.historys = response;
       }
     );
   }
@@ -76,63 +66,12 @@ export class HomepageComponent implements OnInit {
       this.toastrService.info('Please enter a longer search term');
     }
 
-    this.locationService.getLocationCoordinates(this.searchTerm).subscribe(
-      (response: CoordinateModel[]) => {
-        this.lat = response[0].lat;
-        this.lon = response[0].lon;
 
-        const payload = {
-          name: this.searchTerm,
-          date: this.todayDate
-        }
-
-        this.addHistory(payload);
-
-        this.getWeatherDetails();
-      }, (error) => {
-
-      }
-    )
   }
 
-  getWeatherDetails(){
-    const payload = {
-      lat: this?.lat,
-      lon: this?.lon,
-      exclude: 'minutely,hourly,alerts'
-    }
-    this.weatherService.getWeatherData(payload).subscribe(
-      (resp)=> {
-        let weatherData: WeatherModel = resp;
-        weatherData.name = this.capitalizeFirstLetter(this.searchTerm);
-        weatherData.date = this.todayDate;
-
-        this.router.navigate([ApplicationPaths.Detail], {state: {data: weatherData}});
-
-      }, (error) => {
-
-      }
-    )
-  }
-
-  goBack(){
-    this.start = this.start - 9;
-    this.stop = this.stop - 9;
-    this.ngOnInit();
-  }
-
-  goForward(){
-    this.start = this.start + 9;
-    this.stop = this.stop + 9;
-    this.ngOnInit();
-  }
-
-  capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  testClick(history){
-    console.log('div was clicked' + history)
-  }
+  // citySelectedHandler(cityName: string){
+  //   this.searchTerm = cityName;
+  //   this.search();
+  // }
 
 }
